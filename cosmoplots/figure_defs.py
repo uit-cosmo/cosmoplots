b0e083f4f06a3bbedf342292126eb173a578923b
engeir <eirroleng@gmail.com>
2022-02-15 12:46:39 +0100

    Revert back two commits, add set_rcparams_dynamo function

--- cosmoplots/figure_defs.py
+++ cosmoplots/figure_defs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- Encoding: UTF-8 -*-
 
+from typing import List
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -34,25 +35,130 @@ symbol_list_3 = ["^", "o", "v"]
 # 2 symbols in a plot
 symbol_list_2 = ["^", "v"]
 
+# 1 symbol
+symbol_list_1 = ["o"]
+
 
 golden_ratio = 0.5 * (1.0 + np.sqrt(5.0))
 fig_dpi = 300.0
 
 
-def set_rcparams_aip(myParams, num_cols=1, ls="thin"):
+def set_rcparams_dynamo(
+    myParams: mpl.RcParams, num_cols: int = 1, ls: str = "thin"
+) -> List[float]:
+    """Half column width figures for revtex.
+
+    See http://publishing.aip.org/authors/preparing-graphics
+
+    Parameters
+    ----------
+    myParams: matplotlib.RcParams
+        The parameters from matplotlib
+    num_cols: int
+        Number of columns the figure spans.
+        1 column  = 3.37" by 2.08"
+        2 columns = 6.69" by 4.13"
+    ls: str
+        Either 'thick' or 'thin'. Defaults to 'thin'
+    fonts: str
+        Either 'small' or 'large'.
+
+    Returns
+    -------
+    List[float]
+        The size of the axes.
+
+    Raises
+    ------
+    ValueError
+        If the number of columns is not 1 or 2.
     """
-    Half column width figures for revtex, see
-    http://publishing.aip.org/authors/preparing-graphics
-    Input:
-    =======
-    myParams...: matplotlib.rcParams, the parameters from matplotlib
-    num_cols...: int, number of columnes the figure spans.
-                      1 column  = 3.37" by 2.08"
-                      2 columns = 6.69" by 4.13"
-    ls.........: string, either "thick" or "thin". Defaults to "thin"
-    Output:
-    =======
-    None
+
+    fig_dpi = 300.0
+    fontsize = 8
+
+    linewidth = 0.75
+    if ls == "thick":
+        linewidth *= 2
+
+    # Define axis size to be used
+    if num_cols == 1:
+        ax_x0, ax_y0 = 0.2, 0.2
+        axes_size = [ax_x0, ax_y0, 0.95 - ax_x0, 0.95 - ax_y0]
+    elif num_cols == 2:
+        ax_x0, ax_y0 = 0.1, 0.2
+        axes_size = [ax_x0, ax_y0, 0.975 - ax_x0, 0.95 - ax_y0]
+    else:
+        raise ValueError("num_cols must be 1 or 2")
+
+    # Figure size in inch
+    fig_width_in = num_cols * 3.37
+    fig_height_in = 3.37 / golden_ratio
+
+    # Figure size and dpi
+    myParams["figure.dpi"] = fig_dpi
+    myParams["figure.figsize"] = [fig_width_in, fig_height_in]
+    myParams["savefig.dpi"] = fig_dpi
+
+    # Figure legend
+    myParams["legend.framealpha"] = 1.0
+    myParams["legend.fancybox"] = False
+    myParams["legend.edgecolor"] = "k"
+    myParams["patch.linewidth"] = 0.5  # For legend box borders
+    myParams["legend.handlelength"] = 1.45  # Show nice, even
+    # numbers for different line styles
+
+    # Font and text
+    myParams["text.usetex"] = True
+    myParams["pdf.fonttype"] = 42
+    myParams["font.family"] = "Times"
+    myParams["font.size"] = fontsize
+    myParams["axes.labelsize"] = fontsize
+    myParams["legend.fontsize"] = fontsize
+
+    # Line size and marker size
+    myParams["lines.markersize"] = 3.0 * linewidth
+    myParams["lines.linewidth"] = linewidth
+
+    # Axes thickness
+    myParams["axes.linewidth"] = 0.5
+    # Enable minor ticks
+    myParams["ytick.minor.visible"] = True
+    myParams["xtick.minor.visible"] = True
+    # Default ticks on both sides of the axes
+    myParams["xtick.top"] = True
+    myParams["xtick.bottom"] = True
+    myParams["ytick.left"] = True
+    myParams["ytick.right"] = True
+    # All ticks point inward
+    myParams["xtick.direction"] = "in"
+    myParams["ytick.direction"] = "in"
+
+    return axes_size
+
+
+def set_rcparams_aip(
+    myParams: mpl.RcParams, num_cols: int = 1, ls: str = "thin"
+) -> List[float]:
+    """Half column width figures for revtex.
+
+    See http://publishing.aip.org/authors/preparing-graphics
+
+    Parameters
+    ----------
+    myParams: matplotlib.RcParams
+        The parameters from matplotlib
+    num_cols: int
+        Number of columns the figure spans.
+        1 column  = 3.37" by 2.08"
+        2 columns = 6.69" by 4.13"
+    ls: str
+        Either 'thick' or 'thin'. Defaults to 'thin'
+
+    Returns
+    -------
+    List[float]
+        The size of the axes.
     """
 
     assert ls in ["thick", "thin"]
@@ -67,6 +173,8 @@ def set_rcparams_aip(myParams, num_cols=1, ls="thin"):
     elif num_cols == 2:
         ax_x0, ax_y0 = 0.1, 0.2
         axes_size = [ax_x0, ax_y0, 0.95 - ax_x0, 0.95 - ax_y0]
+    else:
+        raise ValueError("num_cols must be 1 or 2")
 
     # Figure size in inch
     fig_width_in = num_cols * 3.37
@@ -77,7 +185,7 @@ def set_rcparams_aip(myParams, num_cols=1, ls="thin"):
     # print 'Figure size: %4.2f"" x %4.2f""' % (fig_width_in, fig_height_in)
     # print 'Figure resolution: %d dpi' % fig_dpi
 
-    # myParams['font.family'] = 'Times'
+    myParams["font.family"] = "Times"
     myParams["font.size"] = 8
     myParams["axes.linewidth"] = 0.5
     myParams["patch.linewidth"] = 0.5  # For legend box borders
@@ -106,7 +214,7 @@ def set_rcparams_aip(myParams, num_cols=1, ls="thin"):
     return axes_size
 
 
-def set_rcparams_article_thickline(myParams):
+def set_rcparams_article_thickline(myParams: mpl.RcParams):
     """
     One 8cm column for the TCV paper, thicker lines for visibility
     """
@@ -119,7 +227,7 @@ def set_rcparams_article_thickline(myParams):
     # print 'Figure size: %4.2f"" x %4.2f""' % (fig_width_in, fig_height_in)
     # print 'Figure resolution: %d dpi' % fig_dpi
 
-    # myParams['font.family'] = 'Time'
+    myParams["font.family"] = "Times"
     myParams["font.size"] = 8
     myParams["axes.linewidth"] = 0.5
     myParams["axes.labelsize"] = 8
@@ -147,7 +255,7 @@ def set_rcparams_article_thickline(myParams):
     myParams["ytick.right"] = True
 
 
-def set_rcparams_poster(myParams):
+def set_rcparams_poster(myParams: mpl.RcParams):
     """
     Make 12.5cm wide figures used in posters
     """
@@ -181,6 +289,7 @@ def set_rcparams_poster(myParams):
 
     myParams["savefig.dpi"] = 300
     myParams["pdf.fonttype"] = 42
+    myParams["font.family"] = "Times"
     myParams["text.usetex"] = True
 
     # Enable minor ticks
@@ -193,7 +302,7 @@ def set_rcparams_poster(myParams):
     myParams["ytick.right"] = True
 
 
-def set_rcparams_talk(myParams):
+def set_rcparams_talk(myParams: mpl.RcParams):
     """
     Matplotlib configuration for talk graphics.
     Use for 16:9 aspect ratio in beamer slides
@@ -204,6 +313,7 @@ def set_rcparams_talk(myParams):
     fig_width_in = 6
     fig_height_in = fig_width_in / golden_ratio
 
+    myParams["font.family"] = "Times"
     myParams["font.size"] = 16
     myParams["axes.linewidth"] = 1.0
     myParams["legend.fontsize"] = 12
